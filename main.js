9c2a994fc89ff9878cee622118e2eb3391cf7192
cameron-chromiak <cam.chromiak@hotmail.com>
2019-02-25 09:56:38 -0500

    -name input works -toggle class desnt work on cell 0 as it should need to add funcction to dlete that row

--- main.js
+++ main.js
@@ -1,64 +1,33 @@
-// let newRowButton = document.getElementById('add-btn')
-//
-// newRowButton.addEventListener('click', insertNewRow)
-//
-// function insertNewRow(){
-//   let table = $('#table')
-//   let row = table.insertRow(-1)
-//
-//   for(let i=0; i<15; i++){
-//     i = row.insertCell(i)
-//   }
-// }
 
-// let cellId = 0
-//
-// $("#addTableRow").click(function() {
-//   var row = $("<tr>");
-//
-//   for (let i = 0; i < 16; i++) {
-//     if (i == 0) {
-//       row.append($("<td>Name.value</td>"))
-//     } else {
-//       row.append($("<td id=" + cellId + " >x</td>"))
-//       cellId += 1
-//     }
-//   }
-//
-//   $("#myTable tbody").append(row);
-// });
-//
-//
-// var table = document.getElementById("myTable");
-// if (table != null) {
-//   for (var i = 0; i < table.rows.length; i++) {
-//     for (var j = 0; j < table.rows[i].cells.length; j++)
-//       table.rows[i].cells[j].onclick = function() {
-//           console.log('run');
-//       getval(this);
-//     };
-//   }
-// }
-  let idCount = 1
+
+let idCount = 1
 
 $('#addRowBtn').click(generateRow)
 
-function generateRow(){
-//   jQuery('<div/>', {
-//     id: `row-${idCount}`,
-//     class: 'generated-row'
-// }).appendTo('#scheduele-container');
 
+
+function generateRow(){
+  var newName = document.getElementById("nameInput").value;
   for(let i=0; i<17; i++){
     jQuery('<div/>',{
       id: `row-${idCount}-cell${i}`,
       class: 'd-inline-block border px-4 py-2 generated-div',
     }).appendTo('#scheduele-container')
   }
+  document.getElementById(`row-${idCount}-cell0`).innerHTML = newName
   jQuery('<br>').appendTo('#scheduele-container')
   idCount +=1
 }
 
+//make first cell not clickable and delete on click
+
+
+
 $('#scheduele-container').on("click", "div", function(){
+  if($(this).attr('id').includes('0')){
+    console.log('delete row');
+  }else{
   $(this).toggleClass('bg-green')
+}
+  // if not a time cell or cell 0 in any row
 });
