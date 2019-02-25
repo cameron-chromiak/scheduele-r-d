d0ac08b577e9f7d3e4d6d8c9a2de6e4fcdaf558c
cameron-chromiak <cam.chromiak@hotmail.com>
2019-02-25 15:08:40 -0500

    SCRAPPED OLD method redesigned As of now: rows dynamically generate depending on object length and are filled with 18 divs per row - each has unique id

--- main.js
+++ main.js
@@ -1,37 +1,59 @@
 
 
-let idCount = 1
 
-$('#addRowBtn').click(generateRow)
+let rowCount = 1
 
+myObj = [{name: "Joe", start: 2 , end: 9},
+  {name: "bob", start: 8 , end: 12}
+]
+  myObj.forEach(function (person) {
+    console.log(person.name, person.start, person.end);
+        jQuery('<div/>',{
+          id: `row-${rowCount}`,
+          class: 'd-block border',
+        }).appendTo('#scheduele-container')
+        addHourDivs(rowCount)
+        rowCount +=1
+  })
 
-
-function generateRow(){
-  var newName = document.getElementById("nameInput").value;
-  for(let i=0; i<17; i++){
-    jQuery('<div/>',{
-      id: `row-${idCount}-cell${i}`,
-      class: `d-inline-block border px-4 py-2 generated-div row${idCount}`,
-    }).appendTo('#scheduele-container')
+  function addHourDivs(rowCount){
+      for(let i=0; i<18; i++){
+        jQuery('<div/>',{
+          id: `row${rowCount}-cell${i}`,
+          class: `d-inline-block generated-div border px-4 py-2`,
+        }).appendTo(`#row-${rowCount}`)
+      }
   }
-  document.getElementById(`row-${idCount}-cell0`).innerHTML = newName
-  jQuery('<br>').appendTo('#scheduele-container')
-  idCount +=1
-}
 
-//make first cell not clickable and delete on click
-
-
-
-$('#scheduele-container').on("click", "div", function(){
-  if($(this).attr('id').includes('0')){
-    let thisRow = $(this).attr('id')[4]
-    thisRow = document.querySelectorAll(`.row${thisRow}`)
-    thisRow.forEach(item =>{
-      item.remove()
-    })
-  }else{
-  $(this).toggleClass('bg-green')
-}
-  // if not a time cell or cell 0 in any row
-});
+// let idCount = 1
+//
+// $('#addRowBtn').click(generateRow)
+//
+// function generateRow(){
+//   var newName = document.getElementById("nameInput").value;
+//   for(let i=0; i<17; i++){
+//     jQuery('<div/>',{
+//       id: `row-${idCount}-cell${i}`,
+//       class: `d-inline-block border px-4 py-2 generated-div row${idCount}`,
+//     }).appendTo('#scheduele-container')
+//   }
+//   document.getElementById(`row-${idCount}-cell0`).innerHTML = newName
+//   jQuery('<br>').appendTo('#scheduele-container')
+//   idCount +=1
+// }
+//
+//
+//
+//
+// $('#scheduele-container').on("click", "div", function(){
+//   if($(this).attr('id').includes('cell0') ){
+//     let thisRow = $(this).attr('id')[4]
+//     thisRow = document.querySelectorAll(`.row${thisRow}`)
+//     thisRow.forEach(item =>{
+//       //need to remove br
+//       item.remove()
+//     })
+//   }else{
+//   $(this).toggleClass('bg-green')
+// }
+// });
