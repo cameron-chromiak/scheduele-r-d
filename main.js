a8001a4d6ed1fa5375bbe0993a3dac092965e6a4
cameron-chromiak <cam.chromiak@hotmail.com>
2019-02-25 17:09:46 -0500

    names appear, formatted -i used a simple if statement in addHourDivs()
    oof

--- main.js
+++ main.js
@@ -4,26 +4,66 @@
 let rowCount = 1
 
 myObj = [{name: "Joe", start: 2 , end: 9},
+{name: "Dale", start: 2 , end: 9},
   {name: "bob", start: 8 , end: 12}
 ]
+
+function main(){
   myObj.forEach(function (person) {
-    console.log(person.name, person.start, person.end);
+    // console.log(person.name, person.start, person.end);
         jQuery('<div/>',{
           id: `row-${rowCount}`,
           class: 'd-block border',
         }).appendTo('#scheduele-container')
-        addHourDivs(rowCount)
+        addHourDivs(rowCount, person)
         rowCount +=1
   })
+}
 
-  function addHourDivs(rowCount){
+
+  function addHourDivs(rowCount, person){
       for(let i=0; i<18; i++){
+        if(i == 0){
+          jQuery('<div/>',{
+            id: `row${rowCount}-cell${i}`,
+            class: `d-inline-block generated-div border px-4 py-2`,
+            text: person.name
+          }).appendTo(`#row-${rowCount}`)
+        }
         jQuery('<div/>',{
           id: `row${rowCount}-cell${i}`,
-          class: `d-inline-block generated-div border px-4 py-2`,
+          class: `d-inline-block generated-div border px-4 py-2`
         }).appendTo(`#row-${rowCount}`)
+          fillRow(person)
       }
   }
+
+
+function fillRow(person){
+
+}
+main()
+
+
+
+
+
+
+
+
+
+// let rows = document.querySelectorAll('.generated-div')
+// let nameCells = []
+// rows.forEach(cell =>{
+//   // console.log(cell);
+//   if ($(cell).attr('id').includes('cell0')){
+//     nameCells.push(cell)
+
+
+
+
+
+
 
 // let idCount = 1
 //
@@ -57,3 +97,32 @@ myObj = [{name: "Joe", start: 2 , end: 9},
 //   $(this).toggleClass('bg-green')
 // }
 // });
+
+////////
+// let rowCount = 1
+//
+// myObj = [{name: "Joe", start: 2 , end: 9},
+// {name: "Joe", start: 2 , end: 9},
+//   {name: "bob", start: 8 , end: 12}
+// ]
+//   myObj.forEach(function (person) {
+//     // console.log(person.name, person.start, person.end);
+//         jQuery('<div/>',{
+//           id: `row-${rowCount}`,
+//           class: 'd-block border',
+//         }).appendTo('#scheduele-container')
+//         addHourDivs(rowCount)
+//         rowCount +=1
+//   })
+//
+//   function addHourDivs(rowCount){
+//       for(let i=0; i<18; i++){
+//         jQuery('<div/>',{
+//           id: `row${rowCount}-cell${i}`,
+//           class: `d-inline-block row generated-div border px-4 py-2`,
+//         }).appendTo(`#row-${rowCount}`)
+//       }
+//       fillNames()
+//   }
+// function fillNames(){
+//   let rows = document.querySelectorAll('.row')
