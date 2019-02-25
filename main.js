f912c5901d687b3ee559c51c5fc45fc173680b96
cameron-chromiak <cam.chromiak@hotmail.com>
2019-02-25 17:28:30 -0500

    -start and end blocks get colored in. the inbetween blocks still need the work tho.

--- main.js
+++ main.js
@@ -3,9 +3,10 @@
 
 let rowCount = 1
 
-myObj = [{name: "Joe", start: 2 , end: 9},
-{name: "Dale", start: 2 , end: 9},
-  {name: "bob", start: 8 , end: 12}
+myObj = [{name: "Jim", start: 1 , end: 9},
+{name: "Bob", start: 2 , end: 9},
+  {name: "Skeeter", start: 3 , end: 7},
+  {name: "Jennylee", start: 8 , end: 12}
 ]
 
 function main(){
@@ -13,7 +14,7 @@ function main(){
     // console.log(person.name, person.start, person.end);
         jQuery('<div/>',{
           id: `row-${rowCount}`,
-          class: 'd-block border',
+          class: 'd-block',
         }).appendTo('#scheduele-container')
         addHourDivs(rowCount, person)
         rowCount +=1
@@ -22,26 +23,38 @@ function main(){
 
 
   function addHourDivs(rowCount, person){
+    let start = person.start
+    let end = person.end
+    let rowDivs
       for(let i=0; i<18; i++){
         if(i == 0){
           jQuery('<div/>',{
             id: `row${rowCount}-cell${i}`,
-            class: `d-inline-block generated-div border px-4 py-2`,
+            class: `d-inline-block generated-div border `,
             text: person.name
           }).appendTo(`#row-${rowCount}`)
+        }else{
+          jQuery('<div/>',{
+            id: `row${rowCount}-cell${i}`,
+            class: `d-inline-block generated-div border px-4 py-2`
+          }).appendTo(`#row-${rowCount}`)
         }
-        jQuery('<div/>',{
-          id: `row${rowCount}-cell${i}`,
-          class: `d-inline-block generated-div border px-4 py-2`
-        }).appendTo(`#row-${rowCount}`)
-          fillRow(person)
       }
+      changeCellColor(start, end)
   }
 
 
-function fillRow(person){
-
+function changeCellColor(start, end){
+  rowDivs = document.querySelectorAll(`#row${rowCount}-cell${start}`)
+  rowDivs.forEach(cell =>{
+    $(cell).toggleClass('bg-green')
+  })
+  rowDivs = document.querySelectorAll(`#row${rowCount}-cell${end}`)
+  rowDivs.forEach(cell =>{
+    $(cell).toggleClass('bg-green')
+  })
 }
+
 main()
 
 
