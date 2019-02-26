2458eed829ae79927563a1e368decee78172036e
cameron-chromiak <cam.chromiak@hotmail.com>
2019-02-26 09:41:21 -0500

    -fixed add bug -rows now update

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-
-// TODO: figure line 18 out
-
-
-
-
 let rowCount = 1
 
 let addBtn = document.getElementById('add-btn')
@@ -16,7 +10,7 @@ function createNewObj(){
   let name = document.getElementById('new-name').value
   myObj.push({name: name, start: startTime, end: endTime})
   clearBoard()
-  main()
+
 }
 
 myObj = [{
@@ -38,12 +32,13 @@ myObj = [{
 }]
 
 function main() {
+  console.log('main');
   myObj.forEach(function(person) {
     // console.log(person.name, person.start, person.end);
     jQuery('<div/>', {
       id: `row-${rowCount}`,
       class: 'd-block',
-    }).appendTo('#scheduele-container')
+    }).appendTo('.row-container')
     addHourDivs(rowCount, person)
     rowCount += 1
   })
@@ -51,8 +46,9 @@ function main() {
 
 function addHourDivs(rowCount, person) {
   //+5 to account for offset
-  let start = person.start + 5
-  let end = person.end + 5
+  console.log('addHourDivs');
+  let start = Number(person.start) + 5
+  let end = Number(person.end) + 5
   let rowDivs
   for (let i = 0; i < 18; i++) {
     if (i == 0) {
@@ -73,18 +69,20 @@ function addHourDivs(rowCount, person) {
 
 
 function changeCellColor(start, end) {
+  console.log('changeCellColor');
   rowDivs = document.querySelectorAll(`#row${rowCount}-cell${start}`)
   while (start < end) {
+    console.log(start, end);
     rowDivs = document.querySelectorAll(`#row${rowCount}-cell${start}`)
     rowDivs.forEach(cell => {
       $(cell).toggleClass('bg-green')
     })
     start += 1
   }
-  let firstCellInRow = document.querySelectorAll('.zero-cell')
+  // let firstCellInRow = document.querySelectorAll('.zero-cell')
 }
 
-$('#scheduele-container').on("click", "div", function() {
+$('.row-container').on("click", "div", function() {
   if ($(this).attr('class').includes('zero')) {
     let thisRow = $(this).attr('id')[3]
     thisRow = document.querySelectorAll(`[id*=row${thisRow}`)
@@ -95,13 +93,12 @@ $('#scheduele-container').on("click", "div", function() {
 })
 
 
-// TODO: Fix
+
 function clearBoard(){
-  let allRows = document.querySelectorAll(`[id*=row`)
-    console.log(allRows);
-  allRows.forEach(elem => {
-    elem.remove()
-  })
+  console.log('clearBoard');
+  $('.row-container').empty()
+
+    main()
 }
 
 main()
